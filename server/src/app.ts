eeefd0fc45c7e4e84f8c6f82d1ae63d0241d11d1
RizqiSugiarto <riskisugiarto290@gmail.com>
2024-07-23 01:41:23 +0700

    adjust cors setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -18,7 +18,7 @@ const app = express()
 const publicPath = path.join(__dirname, '../public/uploads')
 
 const corsOptions = {
-    origin: 'https://your-frontend-domain.com',
+    origin: 'https://master--simpleblogcms.netlify.app/',
     credentials: true,
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-Web-App'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
