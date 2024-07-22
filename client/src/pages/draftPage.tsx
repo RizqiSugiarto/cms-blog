650f22f70df81be389b0f422e090d7f85f44c549
RizqiSugiarto <riskisugiarto290@gmail.com>
2024-07-22 22:48:11 +0700

    adjust import draftPage

--- client/src/pages/draftPage.tsx
+++ client/src/pages/draftPage.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import Table from '@/components/dashboard/table';
 import { useAuthContext } from '@/context/authContext';
 import useGetAllBlogsDraftByUserId from '@/hooks/blog/useGetAllBlogDraft';
